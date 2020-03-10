384e1a03fce3cff06232b4675a83d1b25c8139f5
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-03-10 17:44:52 +0100

    Add direct take mode

--- components/components.js
+++ components/components.js
@@ -2,3 +2,4 @@
 
 import './video/video-cropper.js';
 import './video/source-selector.js';
+import './ui/direct-take-toggle.js';
---
+++ components/ui/direct-take-toggle.js
@@ -0,0 +1,43 @@
+export { tagName, eventNames };
+
+const tagName = 'direct-take-toggle';
+
+const eventNames = {
+	ACTIVATE: 'direct-take-activated',
+	DEACTIVATE: 'direct-take-deactivated'
+};
+
+const classNames = {
+	ARMED: 'armed'
+};
+
+class DirectTakeToggle extends HTMLButtonElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.addEventListener('click', () => {
+			this.handleClick();
+		});
+	}
+
+	handleClick() {
+		if (this.classList.contains(classNames.ARMED)) {
+			this.deactivate();
+		} else {
+			this.activate();
+		}
+	}
+
+	deactivate() {
+		this.classList.remove(classNames.ARMED);
+		this.dispatchEvent(new CustomEvent(eventNames.DEACTIVATE, { composed: true, bubbles: true }));
+	}
+
+	activate() {
+		this.classList.add(classNames.ARMED);
+		this.dispatchEvent(new CustomEvent(eventNames.ACTIVATE, { composed: true, bubbles: true }));
+	}
+}
+customElements.define(tagName, DirectTakeToggle, { extends: 'button' });
