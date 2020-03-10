d59d0df961a4ae8449666884e04f33b5d98855a7
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-03-10 12:02:06 +0100

    wip: build and packaging configuration

--- scripts/package.js
+++ scripts/package.js
@@ -1,21 +1,73 @@
 import packager from 'electron-packager';
 import { readFileSync } from 'fs';
 import { makeRe } from 'micromatch';
+import rimraf from 'rimraf';
+import cpr from 'cpr';
 
-const ignoreGlobs = readFileSync('../.packageignore', 'utf-8')
+const ignore = readFileSync('./.packageignore', 'utf-8')
 	.split(/\r?\n/)
 	.map((glob) => glob.trim())
-	.filter((glob) => glob !== '');
+	.filter((glob) => glob !== '')
+	.map(makeRe);
 
 const options = {
 	dir: 'build',
 	icon: 'assets/sofie_logo',
-	ignore: ignoreGlobs.map(makeRe),
+	ignore,
 	out: 'dist',
 	overwrite: true,
-	prune: true
+	prune: true,
+	quiet: false
 };
 
-packager(options).then((appPaths) => {
-	console.log('Created Electron bundles:', appPaths.join('\n'));
-});
+console.log('Packaging app bundles...');
+
+wipeDir(options.out)
+	.then(() => {
+		return copyFrontendFiles(options.dir);
+	})
+	.then(() => {
+		return packager(options);
+	})
+	.then((appPaths) => {
+		console.log('Created bundles:', appPaths.join('\n'));
+		return true;
+	});
+
+async function wipeDir(dirName) {
+	console.log(`Wiping ${dirName}...`);
+	return new Promise((resolve, reject) => {
+		rimraf(dirName, (err) => {
+			if (err) {
+				console.error('Directory wipe failed!');
+				reject(err);
+			}
+
+			console.log(`${dirName} wiped.`);
+			resolve();
+		});
+	});
+}
+
+async function copyFrontendFiles(targetDir) {
+	const dirsToCopy = ['assets', 'client', 'components', 'lib', 'shared'];
+
+	return Promise.all(
+		dirsToCopy.map((dirName) => {
+			return new Promise((resolve, reject) => {
+				cpr(
+					dirName,
+					`${targetDir}/${dirName}`,
+					{ deleteFirst: true, overwrite: true, confirm: true },
+					(err) => {
+						if (err) {
+							reject(err);
+						}
+
+						resolve();
+					}
+				);
+			});
+		})
+	);
+}
