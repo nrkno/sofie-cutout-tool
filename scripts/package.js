e7ddc6151dea1284b9e119ab847f42dd13ce8e02
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-03-16 17:14:39 +0100

    Non-working with builds to build folder

--- scripts/package.js
+++ scripts/package.js
@@ -1,56 +1,94 @@
-import packager from 'electron-packager';
-import { readFileSync } from 'fs';
-import { makeRe } from 'micromatch';
-import rimraf from 'rimraf';
-import cpr from 'cpr';
+import packager from 'electron-packager'
+import { readFileSync } from 'fs'
+import { makeRe, isMatch } from 'micromatch'
+import rimraf from 'rimraf'
+import cpr from 'cpr'
 
-const ignore = readFileSync('./.packageignore', 'utf-8')
+const dirs = {
+	BUILD: 'build',
+	DIST: 'dist',
+	CONFIG: 'config'
+}
+
+const ignoreGlobs = readFileSync('./.packageignore', 'utf-8')
 	.split(/\r?\n/)
 	.map((glob) => glob.trim())
 	.filter((glob) => glob !== '')
-	.map(makeRe);
+
+console.log('ignores', ignoreGlobs.join('\n'))
 
 const options = {
-	dir: 'build',
+	// asar: {
+	// 	unpackDir: dirs.CONFIG
+	// }
+	dir: dirs.BUILD,
 	icon: 'assets/sofie_logo',
-	ignore,
-	out: 'dist',
+	ignore: (fileName) => {
+		return isMatch(fileName, ignoreGlobs, { basename: true })
+	},
+	out: dirs.DIST,
 	overwrite: true,
 	prune: true,
 	quiet: false
-};
+}
 
-console.log('Packaging app bundles...');
+console.log('Packaging app bundles...')
 
-wipeDir(options.out)
+wipeDir(dirs.DIST)
 	.then(() => {
-		return copyFrontendFiles(options.dir);
+		return copyNodeModules(dirs.BUILD)
 	})
 	.then(() => {
-		return packager(options);
+		return copyFrontendFiles(dirs.BUILD)
+	})
+	.then(() => {
+		return copyConfigFiles(dirs.BUILD)
+	})
+	.then(() => {
+		return packager(options)
 	})
 	.then((appPaths) => {
-		console.log('Created bundles:', appPaths.join('\n'));
-		return true;
-	});
+		console.log('Created bundles:', appPaths.join('\n'))
+		return true
+	})
 
 async function wipeDir(dirName) {
-	console.log(`Wiping ${dirName}...`);
+	console.log(`Wiping ${dirName}...`)
 	return new Promise((resolve, reject) => {
 		rimraf(dirName, (err) => {
 			if (err) {
-				console.error('Directory wipe failed!');
-				reject(err);
+				console.error('Directory wipe failed!')
+				reject(err)
 			}
 
-			console.log(`${dirName} wiped.`);
-			resolve();
-		});
-	});
+			console.log(`${dirName} wiped.`)
+			resolve()
+		})
+	})
+}
+
+async function copyNodeModules(targetDir) {
+	console.log(`Copying node_modules to ${targetDir}`)
+
+	return new Promise((resolve, reject) => {
+		cpr(
+			'node_modules',
+			`${targetDir}/node_modules`,
+			{ deleteFirst: true, overwrite: true, confirm: true },
+			(err) => {
+				if (err) {
+					reject(err)
+				}
+
+				resolve()
+			}
+		)
+	})
 }
 
 async function copyFrontendFiles(targetDir) {
-	const dirsToCopy = ['assets', 'client', 'components', 'lib', 'shared'];
+	const dirsToCopy = ['assets', 'client', 'components', 'lib', 'shared']
+	console.log(`Copying ${dirsToCopy.join(',')} to ${targetDir}`)
 
 	return Promise.all(
 		dirsToCopy.map((dirName) => {
@@ -61,13 +99,32 @@ async function copyFrontendFiles(targetDir) {
 					{ deleteFirst: true, overwrite: true, confirm: true },
 					(err) => {
 						if (err) {
-							reject(err);
+							reject(err)
 						}
 
-						resolve();
+						resolve()
 					}
-				);
-			});
+				)
+			})
 		})
-	);
+	)
+}
+
+async function copyConfigFiles(targetDir) {
+	console.log(`Copying config files to ${targetDir}`)
+
+	return new Promise((resolve, reject) => {
+		cpr(
+			'config',
+			`${targetDir}/config`,
+			{ deleteFirst: true, overwrite: true, confirm: true },
+			(err) => {
+				if (err) {
+					reject(err)
+				}
+
+				resolve()
+			}
+		)
+	})
 }
